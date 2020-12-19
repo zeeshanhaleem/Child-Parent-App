c1c767319995a26a039ab4a13b8bbf50a7ec3195
zeeshanhaleem <zeeshanhaleem142@gmail.com>
2020-12-19 21:19:01 +0500

    Updation in the App.js File

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Restaurant from './restaurant.js';
 function App() {
   return (
     <div className="App">
-   <Restaurant tagofrest= 'is our pride'location='Lahore' signi='Heart'/>
+   <Restaurant tagofrest= 'is our pride 'location='Lahore' signi='Heart'/>
    </div>
   );
 }
